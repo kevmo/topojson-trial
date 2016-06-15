eb51e5e4db81f5b717c29e9b3ea809a3d06034ff
kevmo <k.james.moore@gmail.com>
2016-06-15 11:23:18 -0400

    Switching express default to handlebars.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/countries/:country', function(req, res, next) {
   res.send('respond with a resource');
 });
 
